0bcd2b80aadc4271f4e0170c59930a94b8dc4561
Hammad Rehman <harehman@uwaterloo.ca>
2020-06-14 22:54:02 -0400

    setup Bool theory?

---
+++ Boolean.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+import TheoryInterface as tI
+
+class BooleanConstruct(tI.Construct):
+    def __init__(self, construct):
+        self.allowableConstructs = ["true", "false", "not", "=>", "and", "or", "xor", "par (A)"]
+
+        if construct in self.allowableConstructs:
+            self.construct = construct
+        else:
+            raise Exception("Incorrect construct for type Bool\n")
+
+    def theory(self):
+        return "Core"
+
+    def sort(self):
+        return "Bool"
+
+    def __str__(self):
+        return str(self.construct)
--- TheoryInterface.py
+++ TheoryInterface.py
@@ -1,5 +1,9 @@
-class Construct:  ##derived class BV_ADD, FORALL, AND, ITE, etc.
+from abc import ABC, abstractmethod
+
+class Construct(ABC):  ##derived class BV_ADD, FORALL, AND, ITE, etc.
+
     def __init__(self): pass
+
 
     def theory(self):   pass  # theory construct came from.
 
@@ -8,7 +12,7 @@ class Construct:  ##derived class BV_ADD, FORALL, AND, ITE, etc.
     def __str__(self):  pass  # smt-lib repr
 
 
-class Literal:  ##derived literal class create BV, string, float, etc.
+class Literal(ABC):  ##derived literal class create BV, string, float, etc.
     def __init__(self): pass
 
     def theory(self):   pass  # theory construct came from.
@@ -20,17 +24,17 @@ class Literal:  ##derived literal class create BV, string, float, etc.
     def __str__(self):  pass  # smt-lib repr
 
 
-class ASTNode:  ##AST node class,
+class ASTNode(ABC):  ##AST node class,
     def __init__(self): pass
 
-    self.val = None  # A construct
-    self.children = []  # list of child nodes, [] if leaf
+        #self.val = None  # A construct
+        #self.children = []  # list of child nodes, [] if leaf
 
 
 def __str__(self):  pass
 
 
-class Benchmark:  ##return type of fuzzer.gen, fuzzer.mutate
+class Benchmark(ABC):  ##return type of fuzzer.gen, fuzzer.mutate
     def __init__(self, logic):
         self.logic = None  # i.e, (set-logic LOGIC)
         self.assertions = []  # List of asserting ASTNodes
@@ -38,7 +42,7 @@ class Benchmark:  ##return type of fuzzer.gen, fuzzer.mutate
     def __str__(self):          pass
 
 
-class Fuzzer:
+class Fuzzer(ABC):
     def __init__(self, logic, constructs): pass  ##generates well formed formula over the logic and constructs.
 
     def gen(self, n_assert=5, depth=5): pass
