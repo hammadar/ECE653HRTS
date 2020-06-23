1981816647398021e14af25a8b8bc85db4fd66b7
Hammad Rehman <harehman@uwaterloo.ca>
2020-06-22 21:12:05 -0400

    got negation going

--- smtPair.py
+++ smtPair.py
@@ -1,3 +1,4 @@
+import random
 
 class smtPair:
     lhs = None
@@ -18,18 +19,26 @@ class smtPair:
 
     def outputPair(self):
         expressionToReturn = "(" + self.operation.__str__() + " "
+        left_neg = random.choice([False,True])
+        right_neg = random.choice([False, True])
 
         if (isinstance(self.lhs, smtPair)):
             expressionToReturn += self.lhs.outputPair()
         else:
-            expressionToReturn += self.lhs.__str__()
+            if not left_neg:
+                expressionToReturn += self.lhs.__str__()
+            else:
+                expressionToReturn += "(not " + self.lhs.__str__() + ")"
 
         expressionToReturn += " "
 
         if (isinstance(self.rhs, smtPair)):
             expressionToReturn += self.rhs.outputPair()
         else:
-            expressionToReturn += self.rhs.__str__()
+            if not right_neg:
+                expressionToReturn += self.rhs.__str__()
+            else:
+                expressionToReturn += "(not " + self.rhs.__str__() + ")"
 
         expressionToReturn += ")"
         return expressionToReturn
